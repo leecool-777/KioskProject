61883a8056cc246c4316fcf9fb417e7044fa6770
leecool-777 <siwon0726@naver.com>
2025-04-24 21:34:14 +0900

    [Basic] Lv4 메뉴 클래스 메서드 추가
    addItem, getMenuItems, getCategoryName

--- src/basic/lv4/Menu.java
+++ src/basic/lv4/Menu.java
@@ -1,9 +1,30 @@
 package basic.lv4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
     //속성
+    private String categoryName;
     private List<MenuItem> menuItems;
 
+    //생성자
+    public Menu(String categoryName) {
+        this.categoryName = categoryName;
+        this.menuItems = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item) {
+        menuItems.add(item);
+    }
+
+    public void getMenuItems() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.print(i + 1);
+            System.out.println(". " + menuItems.get(i));
+        }
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
 }
