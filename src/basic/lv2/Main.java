2da3b810ff1a7ec051969dfe0eb4cd3afb3ca1e6
leecool-777 <siwon0726@naver.com>
2025-04-24 19:32:30 +0900

    [Basic] Lv2,3 변수 이름 변경

--- src/basic/lv2/Main.java
+++ src/basic/lv2/Main.java
@@ -1,25 +1,26 @@
 package basic.lv2;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<MenuItem> menuItems = new ArrayList<>();
+        List<MenuItem> burgers = new ArrayList<>();
 
-        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        burgers.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         Scanner scanner = new Scanner(System.in);
         int option;
         while (true) {
-            for (int i = 0; i < menuItems.size(); i++) {
+            for (int i = 0; i < burgers.size(); i++) {
                 System.out.print(i + 1);
-                System.out.println(". " + menuItems.get(i));
-                if (i == menuItems.size() - 1) {
+                System.out.println(". " + burgers.get(i));
+                if (i == burgers.size() - 1) {
                     System.out.println("0. 종료");
                     System.out.println("===================================================================");
                 }
@@ -30,15 +31,15 @@ public class Main {
                 System.out.println("키오스크를 종료합니다.");
                 break;
             } else if (option == 1) {
-                System.out.println(menuItems.get(0));
+                System.out.println(burgers.get(0));
                 break;
             } else if (option == 2) {
-                System.out.println(menuItems.get(1));
+                System.out.println(burgers.get(1));
             } else if (option == 3) {
-                System.out.println(menuItems.get(2));
+                System.out.println(burgers.get(2));
                 break;
             } else if (option == 4) {
-                System.out.println(menuItems.get(3));
+                System.out.println(burgers.get(3));
                 break;
             }
         }
