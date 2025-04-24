784c5eba3a0be3d1c08835ed541fc169aa13824e
leecool-777 <siwon0726@naver.com>
2025-04-24 21:36:33 +0900

    [Basic] Lv4 메인 메서드 생성자 주입

--- src/basic/lv4/Main.java
+++ src/basic/lv4/Main.java
@@ -1,19 +1,24 @@
 package basic.lv4;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<MenuItem> menuItems = new ArrayList<>();
+        Menu burgerMenu = new Menu("BURGERS");
+        burgerMenu.addItem(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgerMenu.addItem(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgerMenu.addItem(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgerMenu.addItem(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        Menu beverageMenu = new Menu("BEVERAGES");
+        beverageMenu.addItem(new MenuItem("Coke", 1.0, "콜라"));
+        beverageMenu.addItem(new MenuItem("Sprite", 1.0, "스프라이트"));
 
-        Kiosk kiosk = new Kiosk(menuItems);
+
+        Kiosk kiosk = new Kiosk();
+        kiosk.addMenu(burgerMenu);
+        kiosk.addMenu(beverageMenu);
+
 
         kiosk.start();
     }
