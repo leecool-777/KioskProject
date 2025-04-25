9c89c8aa09e369eacc20e023c1cb33f06016478b
leecool-777 <siwon0726@naver.com>
2025-04-25 11:28:45 +0900

    [Basic] Lv4 메뉴 메서드 이름 변경 및 메서드 추가
    수정: getMenuItems -> showMenuItems
    추가: menuSize(), getMenuItem()

--- src/basic/lv4/Menu.java
+++ src/basic/lv4/Menu.java
@@ -14,11 +14,12 @@ public class Menu {
         this.menuItems = new ArrayList<>();
     }
 
+    //기능
     public void addItem(MenuItem item) {
         menuItems.add(item);
     }
 
-    public void getMenuItems() {
+    public void showMenuItems() {
         for (int i = 0; i < menuItems.size(); i++) {
             System.out.print(i + 1);
             System.out.println(". " + menuItems.get(i));
@@ -26,5 +27,13 @@ public class Menu {
     }
     public String getCategoryName() {
         return categoryName;
+    }
+
+    public int menuSize() {
+        return menuItems.size();
+    }
+
+    public MenuItem getMenuItem(int i) {
+        return menuItems.get(i-1);
     }
 }
