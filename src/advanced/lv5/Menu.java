3607c1d3834eda049c22a75a60983c05eeeb4df8
leecool-777 <siwon0726@naver.com>
2025-04-25 19:58:13 +0900

    [Feat] Lv5 메뉴 클래스 속성, 생성자, 기능 추가

--- src/advanced/lv5/Menu.java
+++ src/advanced/lv5/Menu.java
@@ -1,4 +1,35 @@
 package advanced.lv5;
 
+import java.util.List;
+
 public class Menu {
+    //속성
+    private String category;
+    private List<MenuItem> menuItems;
+
+    //생성자
+
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    //기능
+    public String getCategory() {
+        return category;
+    }
+
+    public MenuItem getMenuItem(int index) {
+        return menuItems.get(index);
+    }
+
+    public int getMenuSize() {
+        return menuItems.size();
+    }
+
+    public void displayCategoryMenu() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println((i+1) + ". " + menuItems.get(i));
+        }
+    }
 }
