6d339b009159d9953c0c08d470bdfcd36397eb2e
leecool-777 <siwon0726@naver.com>
2025-04-25 11:30:07 +0900

    [Basic] Lv4 키오스크 실행 흐름 개선
    실행흐름 메서드 추가: showMenu(), showItemsOf()

--- src/basic/lv4/Kiosk.java
+++ src/basic/lv4/Kiosk.java
@@ -5,45 +5,62 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
+    //속성
     private List<Menu> menus;
+    Scanner scanner = new Scanner(System.in);
 
+    //생성자
     public Kiosk() {
         this.menus = new ArrayList<>();
     }
 
-
+    //기능
     public void addMenu(Menu menu) {
         menus.add(menu);
     }
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            showMenu();
+
+            System.out.print("메뉴를 선택해주세요: ");
+            int inputMenu = scanner.nextInt();
+            if (inputMenu == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            Menu seletedMenu = menus.get(inputMenu - 1);
+
+            showItemsOf(seletedMenu);
+        }
+    }
+
+    private void showMenu() {
         System.out.println("[ MAIN MENU ]");
         for (int i = 0; i < menus.size(); i++) {
             System.out.println((i + 1) + ". " + menus.get(i).getCategoryName());
         }
         System.out.println("0. 종료");
-
-        while (true) {
-
-            int inputMenu = scanner.nextInt();
-
-            if (inputMenu == 0) {
-                break;
-            } else if (inputMenu == 1) {
-                showItemsOf(menus.get(0));
-            } else if (inputMenu == 2) {
-                showItemsOf((menus.get(1)));
-            }
-
-        }
+        System.out.println("===================================================================");
     }
 
-    public void showItemsOf(Menu menu) {
-        System.out.println("[ " + menu.getCategoryName() + " MENU ]");
-        menu.getMenuItems();
-
+    private void showItemsOf(Menu menu) {
+        while (true) {
+            System.out.println("[ " + menu.getCategoryName() + " MENU ]");
+            menu.showMenuItems();
+            System.out.println("0. 뒤로가기");
+            System.out.println("===================================================================");
+            System.out.print("상품을 선택해주세요: ");
+            int choseItem = scanner.nextInt();
+            if (choseItem == 0) {
+                return;
+            } else if (choseItem > 0 && choseItem <= menu.menuSize()) {
+                System.out.println("선택한 상품: " + menu.getMenuItem(choseItem));
+                System.out.println("===================================================================");
+                return;
+            }
+        }
     }
 }
 
