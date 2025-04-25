7c5d9f16d08ffa3eb571fd8c7f81d47d6ffc2f5c
leecool-777 <siwon0726@naver.com>
2025-04-25 19:55:53 +0900

    [Feat] Lv5 키오스크 클래스 속성, 생성자, 기능 추가
    생성자 주입 방식을 사용하고 메인메뉴 메서드 추가. start 실행흐름 메서드 미구현 진행중

--- src/advanced/lv5/Kiosk.java
+++ src/advanced/lv5/Kiosk.java
@@ -1,4 +1,46 @@
 package advanced.lv5;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Kiosk {
+    //속성
+    private List<Menu> menus;
+    private Order order;
+    private Scanner scanner;
+
+    //생성자
+    public Kiosk(List<Menu> menus, Order order, Scanner scanner) {
+        this.menus = menus;
+        this.order = order;
+        this.scanner = scanner;
+    }
+
+    //기능
+    public void start() {
+        while (true) {
+            displayMainMenu();
+            int inputMenu = scanner.nextInt();
+            if (inputMenu == 0) {
+                break;
+            }
+        }
+
+
+    }
+
+    private void displayMainMenu() {
+        System.out.println("[ MAIN MENU ]");
+        for (int i = 0; i < menus.size(); i++) {
+            System.out.println((i+1) + ". "+ menus.get(i).getCategory());
+        }
+        System.out.println("0. 종료");
+        if (!order.isEmpty()) {
+            System.out.println("\n[ ORDER MENU ]");
+            System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
+            System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
+        }
+        System.out.println("===============================================================");
+
+    }
 }
