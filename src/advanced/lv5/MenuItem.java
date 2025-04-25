dae0c7fbe837210d7c6cbeb5e59e2b860804d474
leecool-777 <siwon0726@naver.com>
2025-04-25 18:11:30 +0900

    [Feat] Lv5 메뉴아이템 클래스 속성, 생성자, 기능 추가

--- src/advanced/lv5/MenuItem.java
+++ src/advanced/lv5/MenuItem.java
@@ -1,4 +1,21 @@
 package advanced.lv5;
 
 public class MenuItem {
+    //속성
+    private String name;
+    private double price;
+    private String info;
+
+    //생성자
+    public MenuItem(String name, double price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    //기능
+    @Override
+    public String toString() {
+        return String.format("%-15s | W %1f | %s",name,price,info);
+    }
 }
