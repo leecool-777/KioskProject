3d205d0eb75270f57db118df3baa7410d150b265
leecool-777 <siwon0726@naver.com>
2025-04-25 17:23:58 +0900

    [Feat] Basic 리팩토링 완료
    키오스크와 메뉴 클래스를 생성자 주입방식으로 변경하고 실행흐름을 개선, 예외처리도입 완료

--- src/basic/ref/Menu.java
+++ src/basic/ref/Menu.java
@@ -1,4 +1,28 @@
 package basic.ref;
 
+import java.util.List;
+
 public class Menu {
+    //속성
+    private String category;
+    private List<MenuItem> menuItems;
+
+    //생성자
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    //기능
+    public String getCategory() {
+        return category;
+    }
+
+    public int getMenuSize() {
+        return menuItems.size();
+    }
+
+    public MenuItem getMenuItem(int index) {
+        return menuItems.get(index);
+    }
 }
