49472fd1aaa346eeaee15298b8307aa2769d3f75
leecool-777 <siwon0726@naver.com>
2025-04-26 16:41:44 +0900

    [Feat] Lv5 키오스크 클래스 실행 흐름 구현 완료 및 변수 이름 수정
    예외처리 완료

--- src/advanced/lv5/Kiosk.java
+++ src/advanced/lv5/Kiosk.java
@@ -1,6 +1,6 @@
 package advanced.lv5;
 
-import javax.management.RuntimeErrorException;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,27 +20,82 @@ public class Kiosk {
     //기능
     public void start() {
         while (true) {
-            displayMainMenu();
-            int inputMenu = scanner.nextInt();
-            if (inputMenu == 0) {
-                break;
-            }
-            if (order.isEmpty() && inputMenu > menus.size()) {
-                System.out.println("잘못 입력하셨습니다.");
-                continue;
-            }
-            Menu selectedMenu = menus.get(inputMenu - 1);
-            displayMenuItem(selectedMenu);
-            int inputItem = scanner.nextInt();
+            try {
 
+
+                //메인 메뉴
+                displayMainMenu();
+                int selectMenu = scanner.nextInt();
+                if (selectMenu == 0) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+                if (order.isEmpty() && selectMenu > menus.size()) {
+                    System.out.println("잘못 입력하셨습니다.");
+                    continue;
+                }
+                if (selectMenu == 4) {
+                    order.getOrders();
+                    System.out.print("\n1. 주문              2. 메뉴판\n입력: ");
+                    int selectOrder = scanner.nextInt();
+                    if (selectOrder == 1) {
+                        System.out.println("주문이 완료되었습니다 ! 금액은 W " + order.getTotalPrice() + " 입니다.");
+                        order.removeAllOrders();
+                        continue;
+                    } else if (selectOrder == 2) {
+                        continue;
+                    }
+                } else if (selectMenu == 5) {
+                    order.removeAllOrders();
+                    System.out.println("장바구니를 비웠습니다 !");
+                    continue;
+                }
+                Menu selectedMenu = menus.get(selectMenu - 1);
+
+                //아이템 메뉴
+                displayMenuItem(selectedMenu);
+
+                int selectItem = scanner.nextInt();
+                MenuItem selectedItem = selectedMenu.getMenuItem(selectItem - 1);
+
+                if (selectItem == 0) {
+                    continue;
+                }
+
+                if (0 < selectItem && selectItem <= selectedMenu.getMenuSize()) {
+                    System.out.println("선택하신 상품: " + selectedItem);
+                }
+                System.out.println("\n" + selectedItem + "\n위 메뉴를 장바구니에 추가하시겠습니까?");
+                System.out.println("1. 확인              2. 취소");
+                System.out.print("입력: ");
+                int inputConfirm = scanner.nextInt();
+
+                switch (inputConfirm) {
+                    case 1:
+                        order.addOrder(selectedItem);
+                        System.out.println(selectedItem.getName() + " 이(가) 장바구니에 추가되었습니다.");
+                        System.out.println("===============================================================");
+                    case 2:
+                        break;
+                    default:
+                        System.out.println("잘못입력하셨습니다");
+                        break;
+                }
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                System.out.println("유효하지 않은 입력입니다. 다시 진행 해주세요");
+                scanner.nextLine();
+            }
             //To do
+            /*
+            장바구니 없을 때 미출력 4,5번 메서드 실행흐름 처리하기
+             */
 
 
         }
     }
 
     private void displayMainMenu() {
-        System.out.println("[ MAIN MENU ]");
+        System.out.println("\n[ MAIN MENU ]");
         for (int i = 0; i < menus.size(); i++) {
             System.out.println((i + 1) + ". " + menus.get(i).getCategory());
         }
@@ -51,9 +106,16 @@ public class Kiosk {
             System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
         }
         System.out.println("===============================================================");
+        System.out.print("입력: ");
     }
 
     private void displayMenuItem(Menu menu) {
-        menu.displayMenuItem(menu);
+        System.out.println("[ " + menu.getCategory() + " MENU ]");
+        for (int i = 0; i < menu.getMenuSize(); i++) {
+            System.out.println((i + 1) + ". " + menu.getMenuItem(i));
+        }
+        System.out.println("===============================================================");
+        System.out.print("입력: ");
     }
 }
+
