dbac3da85c5d459eb0e853b783ed3c927e0a9798
leecool-777 <siwon0726@naver.com>
2025-04-27 12:17:36 +0900

    [Modify] Lv5 Kiosk 클래스 실행 흐름 개선 및 메서드 분리, 이름 수정
    속성 불변객체로 수정.
    변수 이름 변경: selectMenu -> userChoiceMenu
     selectItem -> userChoiceItem
     장바구니 추가 로직 메서드로 분리
     주석 추가.

--- src/advanced/lv5/Kiosk.java
+++ src/advanced/lv5/Kiosk.java
@@ -6,91 +6,82 @@ import java.util.Scanner;
 
 public class Kiosk {
     //속성
-    private List<Menu> menus;
-    private Order order;
-    private Scanner scanner;
+    private final List<Menu> menus;
+    private final Cart cart;
+    private final Scanner scanner;
 
     //생성자
-    public Kiosk(List<Menu> menus, Order order, Scanner scanner) {
+    public Kiosk(List<Menu> menus, Cart cart, Scanner scanner) {
         this.menus = menus;
-        this.order = order;
+        this.cart = cart;
         this.scanner = scanner;
     }
 
     //기능
+    //실행 흐름제어
     public void start() {
         while (true) {
             try {
-
-
                 //메인 메뉴
                 displayMainMenu();
-                int selectMenu = scanner.nextInt();
-                if (selectMenu == 0) {
+
+                int userChoiceMenu = scanner.nextInt();
+
+                if (userChoiceMenu == 0) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 }
-                if (order.isEmpty() && selectMenu > menus.size()) {
+                if (cart.isOrderEmpty() && userChoiceMenu > menus.size()) {
                     System.out.println("잘못 입력하셨습니다.");
                     continue;
                 }
-                if (selectMenu == 4) {
-                    order.getOrders();
-                    System.out.print("\n1. 주문              2. 메뉴판\n입력: ");
-                    int selectOrder = scanner.nextInt();
-                    if (selectOrder == 1) {
-                        System.out.println("주문이 완료되었습니다 ! 금액은 W " + order.getTotalPrice() + " 입니다.");
-                        order.removeAllOrders();
-                        continue;
-                    } else if (selectOrder == 2) {
-                        continue;
-                    }
-                } else if (selectMenu == 5) {
-                    order.removeAllOrders();
-                    System.out.println("장바구니를 비웠습니다 !");
+                //오더 메뉴
+                if (userChoiceMenu == 4 || userChoiceMenu == 5) {
+                    handleOrdersMenu(userChoiceMenu);
                     continue;
                 }
-                Menu selectedMenu = menus.get(selectMenu - 1);
+
+                Menu selectedMenu = menus.get(userChoiceMenu - 1);
 
                 //아이템 메뉴
                 displayMenuItem(selectedMenu);
 
-                int selectItem = scanner.nextInt();
-                MenuItem selectedItem = selectedMenu.getMenuItem(selectItem - 1);
+                int userChoiceItem = scanner.nextInt();
+                MenuItem selectedItem = selectedMenu.getMenuItem(userChoiceItem - 1);
 
-                if (selectItem == 0) {
+                if (userChoiceItem == 0) {
                     continue;
                 }
 
-                if (0 < selectItem && selectItem <= selectedMenu.getMenuSize()) {
-                    System.out.println("선택하신 상품: " + selectedItem);
-                }
-                System.out.println("\n" + selectedItem + "\n위 메뉴를 장바구니에 추가하시겠습니까?");
-                System.out.println("1. 확인              2. 취소");
-                System.out.print("입력: ");
-                int inputConfirm = scanner.nextInt();
+                //장바구니 추가
+                addToCart(userChoiceItem, selectedMenu, selectedItem);
 
-                switch (inputConfirm) {
-                    case 1:
-                        order.addOrder(selectedItem);
-                        System.out.println(selectedItem.getName() + " 이(가) 장바구니에 추가되었습니다.");
-                        System.out.println("===============================================================");
-                    case 2:
-                        break;
-                    default:
-                        System.out.println("잘못입력하셨습니다");
-                        break;
-                }
             } catch (InputMismatchException | IndexOutOfBoundsException e) {
                 System.out.println("유효하지 않은 입력입니다. 다시 진행 해주세요");
                 scanner.nextLine();
             }
-            //To do
-            /*
-            장바구니 없을 때 미출력 4,5번 메서드 실행흐름 처리하기
-             */
+        }
+    }
 
+    private void addToCart(int userChoiceItem, Menu selectedMenu, MenuItem selectedItem) {
+        if (0 < userChoiceItem && userChoiceItem <= selectedMenu.getMenuSize()) {
+            System.out.println("선택하신 상품: " + selectedItem);
+        }
+        System.out.println("\n" + selectedItem + "\n위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인              2. 취소");
+        System.out.print("입력: ");
+        int inputConfirm = scanner.nextInt();
 
+        switch (inputConfirm) {
+            case 1:
+                cart.addItem(selectedItem);
+                System.out.println(selectedItem.getName() + " 이(가) 장바구니에 추가되었습니다.");
+                System.out.println("===============================================================");
+            case 2:
+                break;
+            default:
+                System.out.println("잘못입력하셨습니다");
+                break;
         }
     }
 
@@ -100,7 +91,7 @@ public class Kiosk {
             System.out.println((i + 1) + ". " + menus.get(i).getCategory());
         }
         System.out.println("0. 종료");
-        if (!order.isEmpty()) {
+        if (!cart.isOrderEmpty()) {
             System.out.println("\n[ ORDER MENU ]");
             System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
             System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
@@ -117,5 +108,19 @@ public class Kiosk {
         System.out.println("===============================================================");
         System.out.print("입력: ");
     }
-}
 
+    private void handleOrdersMenu(int selectMenu) {
+        if (selectMenu == 4) {
+            cart.viewCart();
+            System.out.print("\n1. 주문              2. 메뉴판\n입력: ");
+            int selectOrder = scanner.nextInt();
+            if (selectOrder == 1) {
+                System.out.println("주문이 완료되었습니다 ! 금액은 W " + cart.getTotalPrice() + " 입니다.");
+                cart.clearCart();
+            }
+        } else if (selectMenu == 5) {
+            cart.clearCart();
+            System.out.println("장바구니를 비웠습니다 !");
+        }
+    }
+}
