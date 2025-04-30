b00dd6014b5aae70a3d8f166ebc654b31735cf26
leecool-777 <siwon0726@naver.com>
2025-04-30 19:33:30 +0900

    [Feat] Lv6 가격 타입 수정
    double -> int

--- src/advanced/lv6/Cart.java
+++ src/advanced/lv6/Cart.java
@@ -2,15 +2,15 @@ package advanced.lv6;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Cart {
     //속성
     private Map<MenuItem, Integer> cartItems;
     private int totalQuantity;
-    private double totalPrice;
+    private int totalPrice;
 
     //생성자
-
     public Cart() {
         this.cartItems = new HashMap<>();
         this.totalQuantity = 0;
@@ -18,9 +18,10 @@ public class Cart {
     }
 
     //기능
-
-    public Map<MenuItem, Integer> getCartItems() {
-        return cartItems;
+    public void addItem(MenuItem menuItem) {
+        cartItems.put(menuItem, cartItems.getOrDefault(menuItem, 0) + 1);
+        this.totalQuantity++;
+        this.totalPrice += menuItem.getPrice();
     }
 
     public int getTotalQuantity() {
@@ -32,5 +33,23 @@ public class Cart {
     }
     public boolean isEmpty() {
         return cartItems.isEmpty();
+    }
+
+    public int getCartSize() {
+        return cartItems.size();
+    }
+
+    public Set<MenuItem> getCartItems() {
+        return cartItems.keySet();
+    }
+
+    public int getItemQuantity(MenuItem menuItem) {
+        return cartItems.get(menuItem);
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+        this.totalQuantity= 0;
+        this.totalPrice = 0;
     }
 }
