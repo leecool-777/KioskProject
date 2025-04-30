faea59375294b7a4a0a84e23a32299fd82e72367
leecool-777 <siwon0726@naver.com>
2025-04-30 20:31:11 +0900

    [Feat] Lv6 클래스 이름 변경 및 기능 추가
    Discount -> DiscountType
    getter, 할인 금액 반환 기능 추가

---
+++ src/advanced/lv6/DiscountType.java
@@ -0,0 +1,32 @@
+package advanced.lv6;
+
+public enum DiscountType {
+    //속성
+    NATIONAL_MERITORIOUS_PERSON("국가유공자", 10),
+    SOLDIER("군인", 5),
+    STUDENT("학생", 3),
+    GENERAL("일반", 0),
+    ;
+
+
+    private String type;
+    private int discountPercent;
+
+    //생성자
+    DiscountType(String type, int discountPercent) {
+        this.type = type;
+        this.discountPercent = discountPercent;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public int discount(DiscountType discountType, int totalPrice) {
+        return totalPrice * discountPercent / 100;
+    }
+}
